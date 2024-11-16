b39b2823d5ec8931b96edec5c25da84af4d545d8
Himanshu Mahesh <118036094+HimanshuMahesh@users.noreply.github.com>
2024-11-16 22:47:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import BookShelf from './components/BookShelf';
-import { Auth } from './components/Auth';
-
-function PrivateRoute({ children }: { children: React.ReactNode }) {
-  const [user, loading] = useAuthState(auth);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
-
-  return <>{children}</>;
-}
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Auth />} />
-        <Route
-          path="/app"
-          element={
-            <PrivateRoute>
-              <BookShelf />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/app" element={<BookShelf />} />
       </Routes>
     </Router>
   );
